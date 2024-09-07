ee567e378e65050efdb19d4faef9d638350afea8
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-09-07 14:00:56 +0530

    Modify app/models/user.rb

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  # Validations
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :profession, presence: true
+  validates :speciality, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :city, presence: true
+
+  # Callbacks (optional)
+  before_save :capitalize_name
+
+  # Methods (optional)
+  private
+
+  def capitalize_name
+    self.name = name.split.map(&:capitalize).join(' ')
+  end
 end
