051af2ae5202b064190d0f10b8123926eec5ea86
Ivonne <ivonnebenites2015@gmail.com>
2024-09-14 09:56:40 +0200

    Update file App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
-import LoginPage from "./components/auth/LoginPage.jsx";
+import React from 'react';
+import LoginPage from './components/auth/LoginPage.jsx';
+import SignupPage from './components/auth/SignupPage.jsx';
+import ForgotPassword from './components/auth/ForgotPassword.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-      {/* <Header /> */}
-      {/* <FormRegistry /> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   );
 }
 
